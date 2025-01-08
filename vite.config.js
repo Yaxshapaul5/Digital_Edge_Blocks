3b86bd761e15f4efbbff43ac44a00df568fa415d
yaxshapaul5 <yaxshapaull@gmail.com>
2025-01-08 14:55:59 +0530

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'Digital_Edge_Blocks'
-})
+  base: '/Digital_Edge_Blocks/' // Ensure base starts and ends with a slash
+});
